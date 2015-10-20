f9be6ffc2ad625f2e397cea5f8517672ad796982
nrv <nherve@laposte.net>
2015-10-20 18:04:18 +0200

    Final ACMMM2015 prez

--- src/test/java/fr/ina/research/amalia/prez/AmaliaACMMM2015Prez.java
+++ src/test/java/fr/ina/research/amalia/prez/AmaliaACMMM2015Prez.java
@@ -46,15 +46,15 @@ public class AmaliaACMMM2015Prez {
 	public static void main(String[] args) {
 		Prez acm2015 = new Prez("amalia_acm2015");
 		
-		acm2015.add(new PrezImageElement("ppt/Diapositive1.PNG", 10).setText("Title"));
-		acm2015.add(new PrezImageElement("ppt/Diapositive2.PNG", 20).setText("Amalia.js"));
-		acm2015.add(new PrezImageElement("ppt/Diapositive3.PNG", 20).setText("Motivation"));
+		acm2015.add(new PrezImageElement("ppt/Diapositive1.PNG", 20).setText("Title"));
+		acm2015.add(new PrezImageElement("ppt/Diapositive2.PNG", 28).setText("Amalia.js"));
+		acm2015.add(new PrezImageElement("ppt/Diapositive3.PNG", 28).setText("Motivation"));
 		acm2015.add(new PrezImageElement("ppt/Diapositive4.PNG", 20).setText("How"));
 		acm2015.add(new PrezImageElement("ppt/Diapositive5.PNG", 3).setText("Demo"));
 		acm2015.add(new PrezVideoElement("grab_timeline.avi", "00:00:03", "00:00:39").setText("Amalia - simple timeline"));
-		acm2015.add(new PrezVideoElement("grab_zoom.avi", "00:00:14", "00:00:35").setText("Amalia - zoom in timeline"));
+//		acm2015.add(new PrezVideoElement("grab_zoom.avi", "00:00:14", "00:00:35").setText("Amalia - zoom in timeline"));
 		acm2015.add(new PrezImageElement("ppt/Diapositive6.PNG", 5).setText("Research prototypes"));
-		acm2015.add(new PrezVideoElement("grab_diginpix.avi", "00:00:12", "00:00:27").setText("Research prototype - DigInPix"));
+//		acm2015.add(new PrezVideoElement("grab_diginpix.avi", "00:00:12", "00:00:27").setText("Research prototype - DigInPix"));
 		acm2015.add(new PrezVideoElement("grab_speechtrax.avi", "00:00:08", "00:00:45").setText("Research prototype - SpeechTrax"));
 		acm2015.add(new PrezVideoElement("grab_overlay_joconde.avi", "00:00:10", "00:00:31").setText("Research prototype - Object tracking"));
 		acm2015.add(new PrezImageElement("ppt/Diapositive7.PNG", 5).setText("Metadata edition"));
@@ -69,25 +69,26 @@ public class AmaliaACMMM2015Prez {
 		acm2015.add(pce_edit);
 		
 		acm2015.add(new PrezImageElement("ppt/Diapositive8.PNG", 3).setText("Technical details"));
-		acm2015.add(new PrezImageElement("ppt/Diapositive9.PNG", 30).setText("Architecture schema"));
-		acm2015.add(new PrezImageElement("ppt/Diapositive10.PNG", 10).setText("External libraries"));
+		acm2015.add(new PrezImageElement("ppt/Diapositive9.PNG", 38).setText("Architecture schema"));
+		acm2015.add(new PrezImageElement("ppt/Diapositive10.PNG", 8).setText("External libraries"));
 		
-		acm2015.add(new PrezVideoElement("grab_html_integration.avi", "00:00:00", "00:00:42").setText("Amalia - HTML integration and metadata binding"));
+		acm2015.add(new PrezVideoElement("grab_html_integration_2.avi", "00:00:00", "00:00:35").setText("Amalia - HTML integration and metadata binding"));
 		
 		acm2015.add(new PrezImageElement("ppt/Diapositive11.PNG", 5).setText("Documentation & Tools"));
-		acm2015.add(new PrezVideoElement("grab_website_amalia.avi", "00:00:02", "00:00:22").setText("amalia.js web site"));
+		acm2015.add(new PrezVideoElement("grab_website_amalia_2.avi", "00:00:00", "00:00:20").setText("amalia.js web site"));
 		PrezCombinedElement pce_github = new PrezCombinedElement(PrezVideoElement.TIMELINE_BLOCK_ID);
 		pce_github.setText("Amalia - Github projects and model XSD");
-		pce_github.add(new PrezVideoElement("grab_github.avi", "00:00:00", "00:00:05"));
-		pce_github.add(new PrezVideoElement("grab_github.avi", "00:00:16", "00:00:37"));
-		pce_github.add(new PrezVideoElement("grab_github.avi", "00:01:06", "00:01:15"));
+		pce_github.add(new PrezVideoElement("grab_github_2.avi", "00:00:00", "00:00:10"));
+		pce_github.add(new PrezVideoElement("grab_github_2.avi", "00:00:16", "00:00:27"));
+		pce_github.add(new PrezVideoElement("grab_github_2.avi", "00:00:31", "00:00:40"));
+		pce_github.add(new PrezVideoElement("grab_github_2.avi", "00:00:47", "00:00:58"));
 		acm2015.add(pce_github);
 		
 		acm2015.add(new PrezImageElement("ppt/Diapositive12.PNG", 5).setText("Professional applications"));
 		acm2015.add(new PrezVideoElement("grab_poc_sidoc.avi", "00:00:35", "00:01:31").setText("Professional application - segment creation"));
 		
-		acm2015.add(new PrezImageElement("ppt/Diapositive13.PNG", 20).setText("Future work"));
-		acm2015.add(new PrezImageElement("ppt/Diapositive14.PNG", 5).setText("Questions"));
+		acm2015.add(new PrezImageElement("ppt/Diapositive13.PNG", 30).setText("Future work"));
+		acm2015.add(new PrezImageElement("ppt/Diapositive14.PNG", 3).setText("Questions"));
 		
 		PrezGenerator generator = new PrezGenerator().setAvconvCommand(avconv).setTemporaryDir(temp).setDestinationDir(dest).setResourceDir(res);
 		try {
